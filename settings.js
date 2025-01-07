b0343b36dc18b8a2c9bb0f51029224669018cb5c
abdulmalik586 <mabelpedro01@gmail.com>
2025-01-07 07:59:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "opay"
 
-global.banknumber = "8133729715"
+global.banknumber = "8088964614"
 
-global.bankowner = "FADARE"
+global.bankowner = "Legend"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
